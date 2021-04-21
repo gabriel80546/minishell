d18bc1821d1f800d06213551d6256d755f8ed85a
gabriel80546 <gabriel80546@gmail.com>
2021-04-21 18:01:46 -0300

    compiling with -O3

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/dash
-compile="gcc -Wall -Wextra -I libft libft/*.c main.c -o main"
-compile="gcc -Wall -Wextra -g3 -fsanitize=address -I libft libft/*.c main.c -o main"
+compile="gcc -Wall -Wextra -O3 -g3 -fsanitize=address -I libft libft/*.c main.c -o main"
+compile="gcc -Wall -Wextra -O3 -I libft libft/*.c main.c -o main"
 echo $compile
 $compile
